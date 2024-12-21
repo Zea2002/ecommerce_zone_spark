bb2da66a53e44f62fce0b661a5c285a378dd5a9e
unknown <rahmanzea31@gmail.com>
2024-12-22 04:08:45 +0600

    update

--- ecommerce_zone_spark/settings.py
+++ ecommerce_zone_spark/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-4m3j4-ik&o_cinsx5$b4-dyuw**%4%(ru$41t$4t6=)s@vc9y@"
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
