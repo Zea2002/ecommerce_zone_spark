e6569ae498702089a952dc895a935c86b108ec49
unknown <rahmanzea31@gmail.com>
2024-12-22 19:29:36 +0600

    update database

--- ecommerce_zone_spark/settings.py
+++ ecommerce_zone_spark/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','localhost']	
+ALLOWED_HOSTS = ['.vercel.app',]	
 
 
 # Application definition
@@ -91,7 +91,7 @@ CORS_ORIGIN_WHITELIST = (
     'http://localhost:5000',
 )
 CSRF_TRUSTED_ORIGINS = [
-    'https://ecommerce-zone-spark-3hat.vercel.app',
+    'https://ecommerce-zone-spark-6zop.vercel.app/',
     'http://127.0.0.1:8000',
     'http://localhost:3000', # for localhost (REACT Default)
     'http://localhost:5000',
